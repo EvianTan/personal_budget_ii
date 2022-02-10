8d3c92272f21edd3842c4dfa047183de2014e600
Yiyun <yiyuntan.cs@gmail.com>
2022-02-09 17:02:55 -0700

    finish transcation CRUD; no affect envelopes

--- database.sql
+++ database.sql
@@ -8,3 +8,12 @@ CREATE TABLE envelope(
     description VARCHAR(255),
     current_balance DECIMAL(10,2)
 );
+
+CREATE TABLE transcation(
+    transcation_id SERIAL PRIMARY KEY,
+    date DATE,
+    amount DECIMAL(10,2),
+    recipient VARCHAR(255),
+    envelope_id INTEGER,
+    CONSTRAINT envelope FOREIGN KEY (envelope_id) REFERENCES envelope(envelope_id)
+);
--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const pool = require("./db");
 
 app.use(express.json()); // => req.body
 
-// ROUTES //
-
+///////////////////////////////////////////////////////////////////////////////
+// envelope api part
+///////////////////////////////////////////////////////////////////////////////
 // get all envelopes
 app.get("/envelopes", async (req, res) => {
     try {
@@ -81,6 +82,99 @@ app.delete("/envelopes/:id", async (req, res) => {
         );
 
         res.json("Envelope was deleted");
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// transcation api part
+///////////////////////////////////////////////////////////////////////////////
+
+// read all transcation
+app.get("/transcations", async (req, res) => {
+    try {
+        const allTranscations = await pool.query("SELECT * FROM transcation");
+        res.json(allTranscations.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+// read a transcation
+app.get("/transcations/:id", async (req, res) => {  
+    try {
+        const {id} = req.params;
+        const transcation = await pool.query(
+            "SELECT * FROM transcation WHERE transcation_id = $1", 
+            [id]
+        );
+
+        res.json(transcation.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+// create a transcation
+app.post("/transcations", async (req, res) => {
+    try {
+        const date = req.body.date;
+        const amount = req.body.amount;
+        const recipient = req.body.recipient;
+        const envelope_id = req.body.envelope_id;
+
+        const newTranscation = await pool.query(
+            "INSERT INTO transcation (date, amount, recipient, envelope_id) VALUES ($1, $2, $3, $4) RETURNING *",
+            [date, amount, recipient, envelope_id]
+        );
+
+        const updateEnvelope = await pool.query(
+            "UPDATE envelope SET current_balance = current_balance-$1 WHERE envelope_id = $2 RETURNING *",
+            [amount, envelope_id]
+        );
+
+        res.json(newTranscation.rows[0]);
+        
+    } catch(error) {
+        console.error(error.message);
+    }
+})
+
+// update a transcation
+app.put("/transcations/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const date = req.body.date;
+        const amount = req.body.amount;
+        const recipient = req.body.recipient;
+        const envelope_id = req.body.envelope_id;
+
+        // can only update date, recipient
+        const updateTranscation = await pool.query(
+            "UPDATE transcation SET date = $1, amount = $2, recipient = $3, envelope_id = $4 WHERE envelope_id = $5",
+            [date, amount, recipient, envelope_id, id]
+        );
+
+        res.json("Transcation was updated");
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+// delete a transction
+app.delete("/transcations/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deleteTranscation = await pool.query(
+            "DELETE FROM transcation WHERE transcation_id = $1",
+            [id]
+        );
+
+        res.json("Transcation was deleted");
     } catch (error) {
         console.error(error.message);
     }
